ce42c9626b5e5a4d0b2b234b08799156457aedf7
Rinat Zaripov <zaripov.rinat@gmail.com>
2019-09-10 14:18:29 +1000

    fix: remove dev-ops scripts to ab-scripts package

--- template/to-copy/release.config.js
+++ template/to-copy/release.config.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const { DevOps } = require('@aimee-blue/ab-service-kit');
+const { SemanticRelease } = require('@aimee-blue/ab-scripts');
 
 // NOTE: You can run 'yarn run release --dry-run --debug' to see the config
-module.exports = DevOps.buildReleaseConfig({
+module.exports = SemanticRelease.buildReleaseConfig({
   appName: require('./package.json').name,
   docker: true,
   infra: true,
