fb5a5ab3113ec448b33afeab80a7053a5c875287
Jordan Cran <jordan@aimeeblue.com>
2019-10-02 10:55:32 +1000

    chore: export errorInfo, default source and version

--- src/shared/cloudError/sendErrorToCloud.ts
+++ src/shared/cloudError/sendErrorToCloud.ts
@@ -1,10 +1,25 @@
 import { Apps } from '@aimee-blue/ab-contracts';
 import { Subject } from 'rxjs';
 import { Api } from '..';
+import { appName, appVersion } from '../app';
+
+export const errorInfo = Apps.errorInfo;
+
 const errors = new Subject<Apps.ILogErrorParams>();
 
-export const registerError = (error: Apps.ILogErrorParams) =>
-  errors.next(error);
+// What is the best way to start these calls as early as possible so not even the first error has to wait for them?
+const defaultSource = appName();
+const defaultVersion = appVersion();
+
+type registerErrorParams = Pick<Apps.ILogErrorParams, 'error'> &
+  Partial<Apps.ILogErrorParams>;
+
+export const registerError = async (params: registerErrorParams) => {
+  const source = params.source ? params.source : await defaultSource;
+  const version = params.version ? params.version : await defaultVersion;
+
+  errors.next({ error: params.error, source, version });
+};
 
 errors.subscribe(async error => {
   const api = Api.apiOf<Apps.IApi>();
