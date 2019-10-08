e063ecd6e3ed4d8a1e204de24a4d9ed91c02000a
Jordan Cran <jordan@aimeeblue.com>
2019-10-08 16:23:51 +1100

    chore: update ab-shared, use onceAsnc for appName/appVersion

--- src/shared/epics/cloudError.ts
+++ src/shared/epics/cloudError.ts
@@ -2,11 +2,11 @@ import { Apps } from '@aimee-blue/ab-contracts';
 import { from, empty } from 'rxjs';
 import { mergeMap, catchError, ignoreElements } from 'rxjs/operators';
 import { fromEventBus } from '../eventBus';
-import { ofType, appName, appVersion } from '../..';
+import { ofType } from '../ofType';
+import { appName, appVersion } from '../app';
 import { appsLogError } from '../apps';
 
 const sendError = async (errorAction: Apps.IErrorAction) => {
-  // TODO: use new callOnce util.
   const [source, version] = await Promise.all([appName(), appVersion()]);
   return appsLogError({
     error: errorAction.payload,
@@ -14,6 +14,7 @@ const sendError = async (errorAction: Apps.IErrorAction) => {
     version,
   });
 };
+
 export const sendAppErrorsToCloud = () => {
   return fromEventBus().pipe(
     ofType<Apps.IErrorAction>(Apps.ERROR),
